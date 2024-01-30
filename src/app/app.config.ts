a177aaa1f0209e95814589f83ef7baac32606db3
tukuanchung <tukuanchung@gmail.com>
2024-01-30 10:20:57 +0800

    示範關閉 Angular Animations 的方法，使用 provideNoopAnimations()

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideAnimations, provideNoopAnimations} from '@angular/platform-browser/animations';
+// 關掉動畫，讓component正常運作
+// import {provideNoopAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-    provideAnimations()
+    provideAnimations(),
+    // provideNoopAnimations(),
   ],
 
 };
