422357a9acfb50ca1d9bd3fd8d6c3cb0d29f9251
tukuanchung <tukuanchung@gmail.com>
2024-01-30 11:05:39 +0800

    加入民國年的日曆

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,61 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import {provideNativeDateAdapter, DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MatMomentDateAdapterOptions, MomentDateAdapter } from '@angular/material-moment-adapter';
+import moment, { Moment } from 'moment';
+import twMoment from 'moment-taiwan';
+
+export class TaiwanDateAdapter extends MomentDateAdapter {
+
+  constructor(locale: string, options: MatMomentDateAdapterOptions) {
+    super(locale, options);
+    moment.locale(locale);
+  }
+
+  override parse(value: any, parseFormat: string): Moment | null {
+    return moment(moment(value).format(parseFormat));
+  }
+
+  override format(date: Moment, displayFormat: string): string {
+    return twMoment(date).format(displayFormat);
+  }
+
+  override getYearName(date: Moment): string {
+    const twYear = twMoment(date).twYear();
+    return (twYear > 0) ? `${twYear}` : '';
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  providers: [provideNativeDateAdapter()],
+  providers: [provideNativeDateAdapter(),
+    {
+      provide: MAT_DATE_LOCALE,
+      useValue: 'zh-TW'
+    },
+    {
+      provide: DateAdapter,
+      useClass: TaiwanDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: {
+        parse: {
+          dateInput: 'tYY/MM/DD',
+        },
+        display: {
+          dateInput: 'tYY/MM/DD',
+          monthYearLabel: 'tYY MMM',
+          dateA11yLabel: 'LL',
+          monthYearA11yLabel: 'tYY MMMM',
+        },
+      }
+    }],
   imports: [ RouterOutlet, MatFormFieldModule, MatDatepickerModule, FormsModule, ReactiveFormsModule, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideAnimations, provideNoopAnimations} from '@angular/platform-browser/animations';
+import {provideAnimations} from '@angular/platform-browser/animations';
 // 關掉動畫，讓component正常運作
 // import {provideNoopAnimations} from '@angular/platform-browser/animations';
 
